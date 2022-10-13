00a216369805587d5a31c09a38d3e56fc4cd83fa
unknown <istiaq.me@gmail.com>
2022-10-13 08:59:20 +0600

    App Model Created

---
+++ database/migrations/2022_10_13_025217_create_apps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apps', function (Blueprint $table) {
+            $table->id();
+            $table->text('title', 255);
+            $table->text('api_key', 255);
+            $table->json('binded_ips');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apps');
+    }
+}
